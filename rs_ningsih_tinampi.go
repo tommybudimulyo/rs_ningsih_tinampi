c86ef0a68f525435f1b543b8afc0ac6e97b010f6
Fikri Prayoga <fikriprayoga007@gmail.com>
2019-11-24 18:03:40 +0700

    Sudah buat login

--- rs_ningsih_tinampi.go
+++ rs_ningsih_tinampi.go
@@ -17,17 +17,9 @@ type responseObject struct {
 	Response string
 }
 
-type updateDataObject struct {
-	Name        string
-	Temperature string
-	Humidity    string
-	OldName     string
-}
-
-type readDataObject struct {
-	Name        string
-	Temperature string
-	Humidity    string
+type userObject struct {
+	Email		string
+	Password	string
 }
 
 //Function Helper
@@ -37,7 +29,7 @@ func initDatabase(database *sql.DB) *sql.Tx {
 		log.Println(err2)
 	}
 
-	stmt, err3 := tx.Prepare("CREATE TABLE IF NOT EXISTS sbmList (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, temperature TEXT, humidity TEXT)")
+	stmt, err3 := tx.Prepare("CREATE TABLE IF NOT EXISTS userList (email TEXT PRIMARY KEY, password TEXT)")
 	if err3 != nil {
 		log.Println(err3)
 	}
@@ -48,260 +40,132 @@ func initDatabase(database *sql.DB) *sql.Tx {
 
 }
 
-func updateResponseParser(request *http.Request) *updateDataObject {
-	body, err0 := ioutil.ReadAll(request.Body)
-	if err0 != nil {
-		log.Println(err0)
-	}
-	var m updateDataObject
-	err1 := json.Unmarshal(body, &m)
-	if err1 != nil {
-		log.Println(err1)
-	}
-
-	return &m
-}
-
-func updateDataAndroid(aName string, aTemperature string, aHumidity string, aOldName string) {
-	isDone := true
-	database, err0 := sql.Open("sqlite3", "./sbm.db")
-	if err0 != nil {
-		log.Println(err0)
-	}
-	tx := initDatabase(database)
-	defer database.Close()
-	defer tx.Commit()
-
-	mName := ""
-	mTemperature := ""
-	mHumidity := ""
-	rows, err1 := tx.Query("SELECT name, temperature, humidity FROM sbmList")
-	if err1 != nil {
-		log.Println(err1)
-	}
-	for rows.Next() {
-		rows.Scan(&mName, &mTemperature, &mHumidity)
-		if mName == aName {
-			if ((aName) != (aOldName)) || ((mTemperature) != (aTemperature)) || ((mHumidity) != (aHumidity)) {
-				if isDone {
-					client := &http.Client{}
-					postData := []byte("{\"to\": \"/topics/update\", \"data\": {\"message\": \"Server data is updated\"}}")
-					req, err := http.NewRequest("POST", "https://fcm.googleapis.com/fcm/send", bytes.NewReader(postData))
-					if err != nil {
-						os.Exit(1)
-					}
-					req.Header.Add("Content-Type", "application/json")
-					req.Header.Add("Authorization", "key=AIzaSyBjpyC3bPaCekMJy81Irf1TxsZAe7CYRP4")
-					resp, err := client.Do(req)
-					defer resp.Body.Close()
-
-					isDone = false
-
-				}
-
-			}
-
-		}
-
-	}
-
-}
-
-func updateDataAndroid2() {
-	client := &http.Client{}
-	postData := []byte("{\"to\": \"/topics/update\", \"data\": {\"message\": \"Server data is updated\"}}")
-	req, err := http.NewRequest("POST", "https://fcm.googleapis.com/fcm/send", bytes.NewReader(postData))
-	if err != nil {
-		os.Exit(1)
-	}
-	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("Authorization", "key=AIzaSyBjpyC3bPaCekMJy81Irf1TxsZAe7CYRP4")
-	resp, err := client.Do(req)
-	defer resp.Body.Close()
-
-}
-
 func main() {
-	http.HandleFunc("/createData", createDataHandler)
-	http.HandleFunc("/readData", readDataHandler)
-	http.HandleFunc("/updateData", updateDataHandler)
-	http.HandleFunc("/updateData2", updateDataHandler2)
-	http.HandleFunc("/deleteData", deleteDataHandler)
+	http.HandleFunc("/register", register)
+	http.HandleFunc("/login", login)
 
 	http.ListenAndServe(":8080", nil)
 }
 
-func createDataHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseMultipartForm(0)
+func register(w http.ResponseWriter, r *http.Request) {
+	if (r.Method != "POST") {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+		
+	}
 
-	mName := r.FormValue("name")
-	mTemperature := r.FormValue("temperature")
-	mHumidity := r.FormValue("humidity")
-	log.Println(mName)
-	log.Println(mTemperature)
-	log.Println(mHumidity)
+	if err0 := r.ParseForm(); err0 != nil {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
 
-	database, err0 := sql.Open("sqlite3", "./sbm.db")
-	if err0 != nil {
-		log.Println(err0)
+	mEmail := r.FormValue("email")
+	mPassword := r.FormValue("password")
+
+	database, err1 := sql.Open("sqlite3", "./rs_ningsih_tinampi.db")
+	if err1 != nil {
+		log.Println(err1)
+		return
 	}
 	tx := initDatabase(database)
 	defer database.Close()
 	defer tx.Commit()
 
-	stmt, err1 := tx.Prepare("INSERT INTO sbmList (name, temperature, humidity) VALUES (?, ?, ?)")
-	if err1 != nil {
-		log.Println(err1)
-	}
-	stmt.Exec(mName, mTemperature, mHumidity)
-	defer stmt.Close()
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
-
-	m2 := responseObject{"Create data success"}
-	b, err2 := json.Marshal(m2)
+	mEmailDatabase := ""
+	isUserMatch := false
+	rows, err2 := tx.Query("SELECT email FROM userList WHERE email=?", mEmail)
 	if err2 != nil {
 		log.Println(err2)
-	}
-	w.Write(b)
-
-	updateDataAndroid2()
-
-}
-
-func readDataHandler(w http.ResponseWriter, r *http.Request) {
-	database, err0 := sql.Open("sqlite3", "./sbm.db")
-	if err0 != nil {
-		log.Println(err0)
-	}
-	tx := initDatabase(database)
-	defer database.Close()
-	defer tx.Commit()
-
-	mName := ""
-	mTemperature := ""
-	mHumidity := ""
-	var mDeviceDataList []readDataObject
-	rows, err1 := tx.Query("SELECT name, temperature, humidity FROM sbmList")
-	if err1 != nil {
-		log.Println(err1)
 	}
 	for rows.Next() {
-		rows.Scan(&mName, &mTemperature, &mHumidity)
-		mDeviceDataList = append(mDeviceDataList, readDataObject{mName, mTemperature, mHumidity})
+		rows.Scan(&mEmailDatabase)
+		isUserMatch = true;
+		break;
 
 	}
+	defer rows.Close
+
+	if (!isUserMatch) {
+		stmt, err2 := tx.Prepare("INSERT INTO userList (email, password) VALUES (?, ?)")
+		if err2 != nil {
+			log.Println(err2)
+			return
+		}
+		stmt.Exec(mEmail, mPassword)
+		defer stmt.Close()
+	}
+
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(200)
 
-	b, err2 := json.Marshal(mDeviceDataList)
+	m2 := responseObject{"Register success"}
+	if(isUserMatch) {
+		m2 = responseObject{"Register failed"}
+
+	}
+	a, err3 := json.Marshal(m2)
+	if err3 != nil {
+		log.Println(err3)
+		return
+	}
+	w.Write(a)
+
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	if (r.Method != "POST") {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+		
+	}
+
+	if err0 := r.ParseForm(); err0 != nil {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	mEmail := r.FormValue("email")
+	mPassword := r.FormValue("password")
+
+	database, err1 := sql.Open("sqlite3", "./rs_ningsih_tinampi.db")
+	if err1 != nil {
+		log.Println(err1)
+	}
+	tx := initDatabase(database)
+	defer database.Close()
+	defer tx.Commit()
+
+	mEmailDatabase := ""
+	mPasswordDatabase := ""
+	isUserMatch := false
+	rows, err2 := tx.Query("SELECT email, password FROM userList WHERE email=?", mEmail)
 	if err2 != nil {
 		log.Println(err2)
 	}
-	w.Write(b)
+	for rows.Next() {
+		rows.Scan(&mEmailDatabase, &mPasswordDatabase)
+		if ((mEmail == mEmailDatabase) && (mPassword == mPasswordDatabase)) {
+			isUserMatch = true;
+			break;
 
-}
+		}
 
-func updateDataHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseMultipartForm(0)
-
-	mName := r.FormValue("name")
-	mTemperature := r.FormValue("temperature")
-	mHumidity := r.FormValue("humidity")
-	mOldName := r.FormValue("oldName")
-
-	database, err0 := sql.Open("sqlite3", "./sbm.db")
-	if err0 != nil {
-		log.Println(err0)
 	}
-	tx := initDatabase(database)
-	defer database.Close()
-	defer tx.Commit()
-
-	stmt, err0 := tx.Prepare("UPDATE sbmList SET name=?, temperature=?, humidity=? WHERE name=?")
-	if err0 != nil {
-		log.Println(err0)
-	}
-	stmt.Exec(mName, mTemperature, mHumidity, mOldName)
-	defer stmt.Close()
+	defer rows.Close
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(200)
 
-	m2 := responseObject{"Update data success"}
-	b, err1 := json.Marshal(m2)
-	if err1 != nil {
-		log.Println(err1)
+	m2 := responseObject{"Login success"}
+	if (!isUserMatch) {
+		m2 := responseObject{"Login failed"}
 	}
-	w.Write(b)
-
-	updateDataAndroid(mName, mTemperature, mHumidity, mOldName)
-}
-
-func updateDataHandler2(w http.ResponseWriter, r *http.Request) {
-	m := updateResponseParser(r)
-
-	database, err0 := sql.Open("sqlite3", "./sbm.db")
-	if err0 != nil {
-		log.Println(err0)
+	a, err3 := json.Marshal(m2)
+	if err3 != nil {
+		log.Println(err3)
+		return
 	}
-	tx := initDatabase(database)
-	defer database.Close()
-	defer tx.Commit()
-
-	stmt, err0 := tx.Prepare("UPDATE sbmList SET name=?, temperature=?, humidity=? WHERE name=?")
-	if err0 != nil {
-		log.Println(err0)
-	}
-	stmt.Exec(m.Name, m.Temperature, m.Humidity, m.OldName)
-	defer stmt.Close()
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
-
-	m2 := responseObject{"Update data success"}
-	b, err1 := json.Marshal(m2)
-	if err1 != nil {
-		log.Println(err1)
-	}
-	w.Write(b)
-
-	updateDataAndroid(m.Name, m.Temperature, m.Humidity, m.OldName)
-
-}
-
-func deleteDataHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseMultipartForm(0)
-
-	mName := r.FormValue("name")
-
-	database, err0 := sql.Open("sqlite3", "./sbm.db")
-	if err0 != nil {
-		log.Println(err0)
-	}
-	tx := initDatabase(database)
-	defer database.Close()
-	defer tx.Commit()
-
-	stmt, err0 := tx.Prepare("DELETE FROM sbmList WHERE name=?")
-	if err0 != nil {
-		log.Println(err0)
-	}
-	stmt.Exec(mName)
-	defer stmt.Close()
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
-
-	m2 := responseObject{"Delete data success"}
-	b, err1 := json.Marshal(m2)
-	if err1 != nil {
-		log.Println(err1)
-	}
-	w.Write(b)
-
-	updateDataAndroid2()
+	w.Write(a)
 
 }
